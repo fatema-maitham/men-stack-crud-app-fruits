ab4dad93e3b646d6c83409be7ce469e97981d308
fatema-maitham <fatemamaitham1@gmail.com>
2026-08-13 10:19:38 +0300

    done

---
+++ server.js
@@ -0,0 +1,82 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MangoDB ${mongoose.connection.name}`);
+})
+
+const Fruit = require('./models/fruit.js');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get("/fruits", async (req, res) => {
+    const allFruits = await Fruit.find();
+    console.log(allFruits);
+    res.render('fruits/index.ejs', { fruits: allFruits });
+});
+
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.ejs');
+});
+
+app.get('/fruits/:fruitId', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render('fruits/show.ejs', { fruit: foundFruit });
+});
+
+app.post('/fruits', async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    }
+    else {
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect('/fruits');
+});
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+});
+
+app.get('/fruits/:fruitId/edit', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/edit.ejs", { fruit: foundFruit });
+});
+
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
